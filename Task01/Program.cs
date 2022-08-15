436d1f864ddab5c9fe55fedd74207d86aa6e07f5
Dmitry <Logan6338@mail.ru>
2022-08-15 18:15:05 +0300

    Задача 1

---
+++ Task01/Program.cs
@@ -0,0 +1,34 @@
+﻿
+
+int x = Convert.ToInt32(Console.ReadLine());
+
+int y = Convert.ToInt32(Console.ReadLine());
+
+int result = FindNumberQuater(x,y);
+
+Console.WriteLine(result);
+
+int FindNumberQuater(int x, int y)
+{
+    if (x > 0 && y > 0)
+    {
+        return 1;
+    }
+
+     if (x < 0 && y > 0)
+    {
+        return 2;
+    }
+
+     if (x < 0 && y < 0)
+    {
+        return 3;
+    }
+
+     if (x > 0 && y < 0)
+    {
+        return 4;
+    }
+
+    throw new Exception("Ввели не правильные данные,");
+}
