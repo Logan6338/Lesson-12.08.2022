4680bd06f5585ba537ad6b158b712753d388e8c3
Dmitry <Logan6338@mail.ru>
2022-08-15 19:38:34 +0300

    Задача 2

---
+++ Task02/Program.cs
@@ -0,0 +1,25 @@
+﻿int number = Convert.ToInt32(Console.ReadLine());
+string result = FindNumber(number);
+Console.WriteLine(result);
+
+string FindNumber(int q)
+{
+    if (q == 1)
+    {
+        return "x > 0, y > 0";
+    }
+    if (q == 2)
+    {
+        return "x < 0, y > 0";
+    }
+     if (q == 3)
+    {
+        return "x < 0, y < 0";
+    }
+    if (q == 4)
+    {
+        return "x > 0, y < 0";
+    }
+
+    throw new Exception("Ввели не правильные данные,");
+}
