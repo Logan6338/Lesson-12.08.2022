2aedd2ad3f00d274caa209ecf70c1e79d93b78c1
Dmitry <Logan6338@mail.ru>
2022-08-15 20:18:53 +0300

    Задача 4

---
+++ Task04/Program.cs
@@ -0,0 +1,13 @@
+﻿string SqrtFind (int N)
+{
+    string result = "";
+    for (int i = 1; i <= N; i++)
+    {
+        result += $"{Math.Pow(i, 2)} ";
+    }
+    return result;
+}
+
+Console.Write("Ввкдите число N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(SqrtFind(N));
