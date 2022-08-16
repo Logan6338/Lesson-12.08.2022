791a95daa896d3b724d7ef9d40c83fbad3e6a8b3
Dmitry <Logan6338@mail.ru>
2022-08-16 18:43:30 +0300

    задача 4

--- Task04/Program.cs
+++ Task04/Program.cs
@@ -11,3 +11,22 @@
 Console.Write("Ввкдите число N: ");
 int N = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine(SqrtFind(N));
+
+//int n = Convert.ToInt32(Console.ReadLine());
+
+//int[] TableSquares(int s)
+//{
+  //  int[] array = new int[s];
+
+    //for (int i = 1; i <= n; i++)
+    //{
+      //  array[i - 1] = i * i;
+    //}
+    //return array;
+//}
+//int[] result = TableSquares(n);
+//for (int i = 0; i < result.Length; i++)
+//{
+  //  Console.WriteLine(result[i]);
+//}
+
